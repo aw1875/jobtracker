c6b892aed27217edbc0f9eab054c7846c1dc4280
aw1875 <aw1875@rit.edu>
2022-10-22 17:22:44 -0400

    Added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import Router from 'next/router';
+import Script from 'next/script';
 import nProgress from 'nprogress';
 
 import '../utils/styles/globals.css';
@@ -18,21 +19,25 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="Job Tracker Web Application" />
         <title>Job Tracker</title>
       </Head>
+
+      {/* Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-7QLN1SPTH7"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-7QLN1SPTH7');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </>
   );
 }
 
 export default MyApp;
-
-// import '../utils/styles/globals.css'
-// import type { AppProps } from 'next/app'
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />
-// }
-
-// export default MyApp
-
-// <meta name="viewport" content="width=device-width, initial-scale=1" />
-// <title>Job Tracker</title>
